0bf892707df092dfc52a2f8e08593c819b22a4ad
Joophos <joop.doele@gmail.com>
2025-10-17 09:52:34 +0200

    added BarChart

--- WürfelStatistikClass.py
+++ WürfelStatistikClass.py
@@ -6,7 +6,7 @@ locale.setlocale(locale.LC_TIME, "deu_deu")
 class StatsClass:
     def __init__(self, path2csv):
         self.raw_data = pd.read_csv(path2csv)
-        self.raw_data["Datum"] = pd.to_datetime(self.raw_data["Datum"], format="mixed")
+        self.raw_data["Datum"] = pd.to_datetime(self.raw_data["Datum"], format="%d/%m/%Y")
         self.raw_data["Nat20"] = pd.to_numeric(self.raw_data["Nat20"])
         self.raw_data["Nat1"] = pd.to_numeric(self.raw_data["Nat1"])
 
@@ -62,6 +62,37 @@ class StatsClass:
 
         plt.figure(figsize=(5,5))
         plt.pie(total, labels=labels, autopct=autopct_abs)
-        plt.title("Gesamte Wüfelverteilung")
+        plt.title("Gesamte Würfelverteilung")
         plt.show()
+
+    def total_barplot(self):
+        df = self.raw_data
+        df["Nat1_neg"] = -df["Nat1"]
+
+        pivot_nat20 = df.pivot(index="Session", columns="Spieler", values="Nat20")
+        pivot_nat1 = df.pivot(index="Session", columns="Spieler", values="Nat1_neg")
+
+
+        fig, ax = plt.subplots(figsize=(9,6))
+
+        pivot_nat20.plot(kind="bar", ax=ax, stacked=True)
+        pivot_nat1.plot(kind="bar", ax=ax, stacked=True)
+
+        ax.axhline(0, color="black", linewidth=1)
+        ax.set_ylabel("Anzahl Würfe")
+        ax.set_title("Nat20 (oben) und Nat1 (unten) pro Session und Spieler")
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles[:4], labels[:4])
+        ax.set_xticklabels(pivot_nat20.index, rotation=45)
+        plt.tight_layout()
+        plt.show()
+
+    def meanSessionIntervall_print(self):
+        df = self.raw_data.sort_values("Datum")
+        df["t_Delta"] = df["Datum"].diff()
+        df_filt = df[df["t_Delta"] > pd.Timedelta(0)]
+        print(df_filt)
+        mean_t = df_filt["t_Delta"].mean()
+        print(mean_t)
+
 
